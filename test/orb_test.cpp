1c864381a805784068d6d71a56f1a84d8046ebd4
zerovoid <zerovoid10@163.com>
2019-11-21 21:00:55 +0800

    update

---
+++ test/orb_test.cpp
@@ -0,0 +1,27 @@
+#include "kinectgrabber.h"
+
+using namespace zerovoid;
+
+int main(int argc, char *argv[]) {
+    KinectLogger *testlogger = new KinectLogger();    
+    if (testlogger->start() != true) {
+        delete testlogger;
+    }
+    KinectGrabber kinect;
+    if (kinect.start() == false) {
+        return -1;
+    }
+    cv::Mat colorMat, depthMat, irMat;
+    while(kinect.shutdown == false) {
+        if (kinect.waitNewFrame(10) == false) {
+            break;
+        }
+        kinect.getFrames(&colorMat, &depthMat, nullptr);
+        cv::imshow("color", colorMat);
+        cv::imshow("dep", depthMat / 4096.0f);
+        //cv::imshow("ir", irMat / 4096.0f);
+        kinect.closeCheck();
+    }
+    kinect.close();
+    return 0;
+}
